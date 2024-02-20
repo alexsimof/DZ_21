1e0b406110b42897e33b8324e47e94e6de52b105
Alex Simonov <alex.simonof@gmail.com>
2024-02-20 23:09:04 +0200

    modified script

--- js/script.js
+++ js/script.js
@@ -117,15 +117,43 @@ button.addEventListener('click', function(){
 //
 const NEWYEAR = 20;
 const BLACKFRIDAY = 30;
-const SUMMERSALE = 15
+const SUMMERSALE = 15;
+const bigCash = 10;
+
+let answeare = document.getElementById('answeare');
 let key = document.getElementById('key');
+
 key.addEventListener('click', function(){
   let cost = document.getElementById('cost').value; // цена
   let code = document.getElementById('code').value; // код скидки
   let qty = document.getElementById('qty').value;  // кол-во товара
 
-  switch (true) {
-    case :
+  switch (!code && !cost && !qty) {
+    case code == 'NEWYEAR':
+      let discount1 = (cost - (cost / 100 * NEWYEAR)) * qty;
+      if (discount1 < 1000) {
+        answeare.innerHTML = `Ваша цена - ${discount1} гривень`
+      } else {
+        let totalPrice1 = discount1 - (discount1 / 100 * bigCash)
+        answeare.innerHTML = `Ваша цена - ${totalPrice1} гривень`
+      }
+      break;
+      
+    case code == 'BLACKFRIDAY':
+      let discount2 = (cost - (cost / 100 * NEWYEAR)) * qty;
+      answeare.innerHTML = `Ваша цена - ${discount2} гривень`
+      break;
+    case code == 'SUMMERSALE':
+      let discount3 = (cost - (cost / 100 * NEWYEAR)) * qty;
+      answeare.innerHTML = `Ваша цена - ${discount3} гривень`
+      break;
+    
+
+
+    default:
+      answeare.innerHTML = 'Заполните все поля'
+      break;
+
   }
 
 
